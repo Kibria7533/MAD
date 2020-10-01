bbd7db74195eb7a7cd861018a3c790b4c1359684
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-10-01 08:28:06 +0600

    1st

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ app.use(passport.initialize());
 
 app.use(bp.json());
 if(process.env.NODE_ENV==="production"){
-  app.use(exp.static(path.join(__dirname,'client/build')));
+  app.use(exp.static('client/build'));
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 
 }
 
